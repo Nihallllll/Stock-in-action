96a0d9af1f539c60bdbb272268d90ed8eea47943
Nihallllll <nihal24rajak@gmail.com>
2025-08-18 13:59:28 +0530

    added logic in collateralVault

--- Contract/src/CollateralVault.sol
+++ Contract/src/CollateralVault.sol
@@ -3,9 +3,24 @@ pragma solidity ^0.8.13;
 
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import "@IPriceOracle.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+
 contract CollateralVault {
+  using SafeERC20 for IERC20;
   
-  
+  struct Tokens {
+    address _token;
+    uint _quantity;
+  }
+  mapping(address =>uint ) public 
+  mapping(address => Tokens) public collateralBalanceofBorrower;
+
+  function depositeCollateral(address _token , uint _amount) external {
+    require(amount > 0);
+    IERC20(_token).safeTransferFrom(msg.sender,address(this),_amount);
+    collateralBalanceofBorrower[msg.sender]._token = _token;
+    collateralBalanceofBorrower[msg.sender]._amount = _amount;
+  }
   
 }
--- Contract/src/LendingPool.sol
+++ Contract/src/LendingPool.sol
@@ -1,51 +1,53 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract LendingPool {
+    using SafeERC20 for IERC20;
     //state variables
     uint public totalPoolBalance;
-    IERC20 public musdc;
-    uint constant public BONUS_% = 8 ;
+    IERC20 public immutable musdc;
+    uint constant public BONUS = 8 ;
     uint constant public INTEREST_RATE = 10 ;
     struct LenderPosition {
         uint _time ;
         uint _depositAmount;
     }
+
     //constructor
 
     constructor(address _musdc){
         musdc = IERC20(_musdc);
     }
     //mappings
-    mapping(address => LenderPosition ) lendersBalances;
+    mapping(address => LenderPosition )public  lendersBalances;
     mapping (address => uint) borrowerBalances;
     //Lenders functions
 
     function deposite(address _token,uint _amount) external {
         require(_amount > 0);
         require(_token == address(musdc), "Invalid token");
-        musdc.transferFrom(msg.sender, address(this), _amount);//require??
+        musdc.safeTransferFrom(msg.sender, address(this), _amount);//require??
         totalPoolBalance += _amount;
         lendersBalances[msg.sender]._depositAmount += _amount;
     }
 
     function withdraw(address _token,uint _amount) external {
-        LenderPosition storage position = LenderPosition[msg.sender];
+       LenderPosition storage position = lendersBalances[msg.sender];
         require(_amount > 0 && _amount <= position._depositAmount);
-        require(_token == musdc);
+        require(IERC20(_token) == musdc);
         uint totalBenefit = getLenderWithdrawAmount(msg.sender);
         musdc.transfer(msg.sender , totalBenefit);
         position._depositAmount -= _amount;
         position._time = block.timestamp;
         totalPoolBalance -= totalBenefit ;
     }
-
+ 
     function getLenderWithdrawAmount(address _address) internal returns(uint){
-        LenderPosition storage position = LenderPosition[_address];
+        LenderPosition storage position = lendersBalances[_address];
         uint timeElapsed = block.timestamp - position._time ;
         require(timeElapsed > 1 days);
-        uint totalBenefit = (position._depositAmount * BONUS_% )/100 ;
+        uint totalBenefit = (position._depositAmount * BONUS )/100 ;
         return totalBenefit;
     }
 
@@ -63,7 +65,7 @@ contract LendingPool {
     function repay(address _token , uint _amount) external{
      require(borrowerBalances[msg.sender]  >= _amount);
      require(_amount != 0);
-    IERC20( _token).transferFrom(msg.sender , address(this) , _amount);
+    IERC20( _token).safeTransferFrom(msg.sender , address(this) , _amount);
      borrowerBalances[msg.sender] -= _amount;
      totalPoolBalance += _amount ;
     }
